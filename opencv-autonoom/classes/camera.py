bd6e2b55488712daefa278585ba24e34be48d4a0
W03Fi3 <larsh.hanssen@gmail.com>
2017-06-09 16:28:51 +0200

    final line detection code

--- opencv-autonoom/classes/camera.py
+++ opencv-autonoom/classes/camera.py
@@ -3,64 +3,51 @@ import cv2
 import time
 import math
 import urllib
+import socket
 
-lijn1 = None
-lijn2 = None
+
 
 def draw_lines(img, lines):
-    
+    lijn1 = None
+    lijn2 = None
 
     i = 0
-    print lines
-    centerpic = 300
     # print lines
+    centerpic = 300
 
     try:
-        while lijn1 is None and lijn2 is None:
-            # eerste lijn en die moet onder de 350 pixels met de x1 en x2
+        while (lijn1 is None and lijn2 is None):
+            # eerste lijn en die moet onder de 300 pixels met de x1 en x2
             # eerste lijn niet twee keer vullen
             if lijn1 is None:
-                a = i
+                if lijn2 is None:
+                    a = i
                 if lines[i][0][0] < centerpic and lines[i][0][2] < centerpic:
                     lijn1 = lines[i][0]
+                    cv2.line(img, (lijn1[0], lijn1[1]), (lijn1[2], lijn1[3]), [0, 255, 0], 3)
                     a += 1
                     # print "dit is lijn1"
                     # print lijn1
-            # tweede lijn moet boven de 400 pixels met beide x1 en x2
+            # tweede lijn moet boven de 300 pixels met beide x1 en x2
             # tweede lijn niet opnieuw vullen
             if lijn2 is None:
                 if lines[a][0][0] >= centerpic and lines[a][0][2] >= centerpic:
                     lijn2 = lines[a][0]
+                    cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)
                     # print "dit is lijn2"
                     # print lijn2
-        i += 1
-        cv2.line(img, (lijn1[0], lijn1[1]), (lijn1[2], lijn1[3]), [0, 255, 0], 3)
+            i += 1
 
-    # if lijn2 is not None:
-        cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)
         xtop = (lijn2[0] + lijn1[2]) / 2
         xbot = (lijn2[2] + lijn1[0]) / 2
-        cv2.line(img,(xtop, 0), (xbot, 400),[255, 140, 0], 3)
-        print 'midden'
-        middle = (xbot+xtop)/2
-        print middle
-
-
-        cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)
-        xtop = (lijn2[0] + lijn1[2]) / 2
-        xbot = (lijn2[2] + lijn1[0]) / 2
+        # cv2.line(img,(xtop, 0), (xbot, 400),[255, 140, 0], 3)
+        # cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)
         gem = (xtop + xbot) / 2
+        # print gem
         draw_middle(img, gem)
         cv2.line(img,(gem, 0), (gem, 400),[255, 140, 0], 3)
 
 
-        # print xtop
-        # print xbot
-
-
-
-       # calculate_degree(lijn1)
-       # calculate_degree(lijn2)
     except Exception:
         pass
 
@@ -68,8 +55,12 @@ def draw_lines(img, lines):
 def draw_middle(img, gem):
     y, x, z = img.shape
     dif = gem - (x / 2)
-
-
+    TCP_IP = '192.168.42.1'
+    TCP_PORT = 5005
+    BUFFER_SIZE = 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+    s.send('test')
     print dif
     cv2.line(img, ((x / 2), y), ((x/2), y-50), [85, 26, 139], 1)
 
@@ -97,7 +88,7 @@ def process_img(original_image):
     vertices = np.array([[0, 400], [0, 200], [300, 200], [500, 200], [800, 200], [800, 400]], np.int32)
     processed_img = roi(processed_img, [vertices])
     #                       edges
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 50, 15)
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 30, 50)
     draw_lines(original_image, lines)
     return processed_img
 
