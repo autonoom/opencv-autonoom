1a427a1fe00b5a02f96532bcc391e50d10608843
W03Fi3 <larsh.hanssen@gmail.com>
2017-06-16 17:19:32 +0200

    small change to camera.py

--- opencv-autonoom/classes/camera.py
+++ opencv-autonoom/classes/camera.py
@@ -1,42 +1,43 @@
 import numpy as np
 import cv2
-import time
 import math
 import urllib
 import socket
 
 
-
 def draw_lines(img, lines):
     lijn1 = None
     lijn2 = None
-
     i = 0
     # print lines
     centerpic = 300
-
     try:
-        while (lijn1 is None and lijn2 is None):
+        while not(i is len(lines)):
             # eerste lijn en die moet onder de 300 pixels met de x1 en x2
             # eerste lijn niet twee keer vullen
-            if lijn1 is None:
-                if lijn2 is None:
-                    a = i
-                if lines[i][0][0] < centerpic and lines[i][0][2] < centerpic:                   # als de x-coördinaat onder de 300 pixels ligt dan is dat lijn 1
+
+            # print lines[i][0]
+            if lines[i][0][0] < centerpic and lines[i][0][2] < centerpic:
+                if lijn1 is None:
                     lijn1 = lines[i][0]
-                    cv2.line(img, (lijn1[0], lijn1[1]), (lijn1[2], lijn1[3]), [0, 255, 0], 3) # Teken lijn 1
-                    a += 1
-                    # print "dit is lijn1"
-                    # print lijn1
+                else:
+                    lijn1 = (lijn1 + lines[i][0]) / 2
+
+                # print "dit is lijn1"
+                # print lijn1
             # tweede lijn moet boven de 300 pixels met beide x1 en x2
             # tweede lijn niet opnieuw vullen
-            if lijn2 is None:
-                if lines[a][0][0] >= centerpic and lines[a][0][2] >= centerpic:             # als de x-coördinaat boven de 300 pixels ligt dan is dat lijn 2
-                    lijn2 = lines[a][0]
-                    cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)   # teken lijn 2
-                    # print "dit is lijn2"
-                    # print lijn2
+            if lines[i][0][0] >= centerpic and lines[i][0][2] >= centerpic:
+                if lijn2 is None:
+                    lijn2 = lines[i][0]
+                else:
+                    lijn2 = (lijn2 + lines[i][0]) / 2
+
+                # print "dit is lijn2"
+                # print lijn2
             i += 1
+        cv2.line(img, (lijn1[0], lijn1[1]), (lijn1[2], lijn1[3]), [0, 255, 0], 2)  # Teken lijn 1
+        cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 2)  # teken lijn 2
             # bereken gemiddelde van per lijn lijn
         xtop = (lijn2[0] + lijn1[2]) / 2
         xbot = (lijn2[2] + lijn1[0]) / 2
@@ -45,26 +46,43 @@ def draw_lines(img, lines):
         gem = (xtop + xbot) / 2         # berekent gemiddelde van de 2 lijnen
         # print gem
         draw_middle(img, gem)
-        cv2.line(img,(gem, 0), (gem, 400),[255, 140, 0], 3)
-
+        cv2.line(img, (gem, 0), (gem, 400), [255, 140, 0], 3)
 
     except Exception:
         pass
 
-    # hier word het verschil berekent tussen het midden van de camera en het gemiddelde van de twee lijnen
-def draw_middle(img, gem):
-    y, x, z = img.shape
+def draw_circles(img):
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+    circles = np.uint16(np.around(circles))
+
+    for i in circles[0, :]:
+        if i[2] < 200:
+            rad = (i[2] + rad) / 2
+            x = (i[0] + x) / 2
+            y = (i[1] + y) / 2
+
+            # draw the outer circle
+            cv2.circle(img, (y, x), rad, (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+            # print i
+
+
+def draw_middle(image, gem):
+    y, x, z = image.shape
     dif = gem - (x / 2)
+    # print x
+    # print y
 
     # hier wordt her verschil van het gemiddelde en het midden van de camera opgestuurd via telnet
-    TCP_IP = '192.168.42.1'
-    TCP_PORT = 5005
-    BUFFER_SIZE = 1024
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    s.send(dif)
+    # TCP_IP = '192.168.42.1'
+    # TCP_PORT = 5005
+    # BUFFER_SIZE = 1024
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.connect((TCP_IP, TCP_PORT))
+    # s.send(dif)
     print dif
-    cv2.line(img, ((x / 2), y), ((x/2), y-50), [85, 26, 139], 1)
+    cv2.line(image, ((x / 2), y), ((x/2), y-50), [85, 26, 139], 1)
 
 
 # dit is code om de hoek te bepalen van de lijnen dit gebruiken wij niet
@@ -75,7 +93,19 @@ def calculate_degree(point):                     # http://wikicode.wikidot.com/g
     angle = angle * -1
     print round(angle % 360)
 
-                                                                # maakt region of interest aan
+
+def bird_eye (image):
+    # y, x, ch = image.shape
+    # print x
+    # print y
+    src = np.float32([[0, 140], [480, 140], [480, 360], [0, 360]])
+    dst = np.float32([[0, 0], [480, 0], [300, 450], [180, 450]])
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped_image = cv2.warpPerspective(image, M, (640, 480))
+    cv2.imshow('transform', warped_image)
+
+# maakt region of interest aan
+
 def roi(img, vertices):
     mask = np.zeros_like(img)
     cv2.fillPoly(mask, vertices, 255)
@@ -84,20 +114,22 @@ def roi(img, vertices):
 
 
 def process_img(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)                                        # maakt van de originele image een GRAY scale foto
-    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)                                # laat de buitenste lijnen zien van een object
-    processed_img = cv2.GaussianBlur(processed_img, (3, 3), 0)                                              # blur die image!!!
-    vertices = np.array([[0, 400], [0, 200], [300, 200], [500, 200], [800, 200], [800, 400]], np.int32)     # bepaal de region of interest
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    processed_img = cv2.GaussianBlur(processed_img, (3, 3), 0)
+    vertices = np.array([[0, 400], [50, 200], [150, 150], [500, 150], [800, 400], [800, 400]], np.int32)
     processed_img = roi(processed_img, [vertices])
     #                       edges
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 30, 50)                         # berekent lijnen van de image door houghlineP
-    draw_lines(original_image, lines)                                                                       # stuurt de foto en de coordinaten van de lijnen op
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 50, 50)
+    draw_lines(original_image, lines)
+
+    # cv2.imshow('sjaak', original_image)         # stuurt de foto en de coordinaten van de lijnen op
     return processed_img
 
 
 def main():
-    last_time = time.time()                             # https://github.com/miguelgrinberg/flask-video-streaming
-    stream = urllib.urlopen('http://192.168.42.1:5000/video_feed')                                          # haalt video stream op
+    # https://github.com/miguelgrinberg/flask-video-streaming
+    stream = urllib.urlopen('http://192.168.42.1:5000/video_feed')                           # haalt video stream op
     bytes = ''
     while True:
         bytes += stream.read(1024)
@@ -106,14 +138,12 @@ def main():
         if a != -1 and b != -1:
             jpg = bytes[a:b + 2]
             bytes = bytes[b + 2:]
-            frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)           # frame is een frame van de video stream
-            new_screen = process_img(frame)                                                     # stuurt frame op naar process_img
-           # print('Loop took {} fps' .format(1 / (time.time()-last_time)))
-            last_time = time.time()
+            frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            new_screen = process_img(frame)                                   # stuurt frame op naar process_img
             cv2.imshow('window', new_screen)                                        # laat video zien
             # cv2.imshow('wiqndow2', cap)
             cv2.imshow('window2', frame)
-            if cv2.waitKey(25) & 0xFF == ord('q'):                              # wanneer 'q' is ingedrukt stop programma
+            if cv2.waitKey(25) & 0xFF == ord('q'):                         # wanneer 'q' is ingedrukt stop programma
                 cv2.destroyAllWindows()
                 break
 main()
--- tests/more-test.py
+++ tests/more-test.py
@@ -1,62 +1,72 @@
-import cv2
 import numpy as np
+import cv2
 import time
 import math
+import urllib
+import socket
 
-centerPic = 350
 
 def draw_lines(img, lines):
     lijn1 = None
     lijn2 = None
     i = 0
+    # print lines
+    centerpic = 300
     try:
-        while lijn1 is None and lijn2 is None:
-            #eerste lijn en die moet onder de 350 pixels met de x1 en x2
-            #eerste lijn niet twee keer vullen
-            if lijn1 is None:
-                a = i
-                if lines[i][0][0] < centerPic and lines[i][0][2] < centerPic:
+        while not(i is len(lines)):
+            # eerste lijn en die moet onder de 300 pixels met de x1 en x2
+            # eerste lijn niet twee keer vullen
+
+            if lines[i][0][0] < centerpic and lines[i][0][2] < centerpic:
+                if lijn1 is None:
                     lijn1 = lines[i][0]
-                    print "dit is lijn1"
-                    print lijn1
-                    a += 1
-
-            #tweede lijn moet boven de 350 pixels met beide x1 en x2
-            #tweede lijn niet opnieuw vullen
-            if lijn2 is None:
-                if lines[a][0][0] >= centerPic and lines[a][0][2] >= centerPic:
-                    lijn2 = lines[a][0]
-                    print "dit is lijn2"
-                    print lijn2
+                else:
+                    lijn1 = (lijn1 + lines[i][0]) / 2
+                cv2.line(img, (lijn1[0], lijn1[1]), (lijn1[2], lijn1[3]), [0, 255, 0], 2)  # Teken lijn 1
+                # print "dit is lijn1"
+                # print lijn1
+            # tweede lijn moet boven de 300 pixels met beide x1 en x2
+            # tweede lijn niet opnieuw vullen
+            if lines[i][0][0] >= centerpic and lines[i][0][2] >= centerpic:
+                if lijn2 is None:
+                    lijn2 = lines[i][0]
+                else:
+                    lijn2 = (lijn2 + lines[i][0]) / 2
+                cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 2)   # teken lijn 2
+                # print "dit is lijn2"
+                # print lijn2
             i += 1
-            #elif lijn2 is None or (coords[0] < lijn1[0] -100 or coords[0] > lijn1[0] + 100):
-            #    coords = line[0]
-            #if lijn1 is None:
-            #    lijn1 = coords
-            #elif lijn2 is None and lijn1 is not None:
-            #    lijn2 = coords
 
-            cv2.line(img, (lijn1[0], lijn1[1]), (lijn1[2], lijn1[3]), [0, 255, 0], 3)
-            if lijn2 is not None:
-                cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)
+            # bereken gemiddelde van per lijn lijn
         xtop = (lijn2[0] + lijn1[2]) / 2
         xbot = (lijn2[2] + lijn1[0]) / 2
-        cv2.line(img,(xtop, 0), (xbot, 400),[255, 140, 0], 3)
-        print xtop
-        print xbot
+        # cv2.line(img,(xtop, 0), (xbot, 400),[255, 140, 0], 3)
+        # cv2.line(img, (lijn2[0], lijn2[1]), (lijn2[2], lijn2[3]), [0, 255, 0], 3)
+        gem = (xtop + xbot) / 2         # berekent gemiddelde van de 2 lijnen
+        # print gem
+        draw_middle(img, gem)
+        cv2.line(img, (gem, 0), (gem, 400),[255, 140, 0], 3)
 
-        calculate_degree(lijn1)
-        calculate_degree(lijn2)
     except Exception:
         pass
 
+    # hier word het verschil berekent tussen het midden van de camera en het gemiddelde van de twee lijnen
+def draw_middle(image, gem):
+    y, x, z = image.shape
+    dif = gem - (x / 2)
 
-def draw_middle(img):
-    y, x, z = img.shape
-    cv2.line(img, ((x / 2), y), ((x/2), y-50), [85, 26, 139], 3)
+    # hier wordt her verschil van het gemiddelde en het midden van de camera opgestuurd via telnet
+    # TCP_IP = '192.168.42.1'
+    # TCP_PORT = 5005
+    # BUFFER_SIZE = 1024
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.connect((TCP_IP, TCP_PORT))
+    # s.send(dif)
+    print dif
+    cv2.line(image, ((x / 2), y), ((x/2), y-50), [85, 26, 139], 1)
 
 
-
+# dit is code om de hoek te bepalen van de lijnen dit gebruiken wij niet
 def calculate_degree(point):                     # http://wikicode.wikidot.com/get-angle-of-line-between-two-points
     x_diff = point[2] - point[0]
     y_diff = point[3] - point[1]
@@ -64,16 +74,25 @@ def calculate_degree(point):                     # http://wikicode.wikidot.com/g
     angle = angle * -1
     print round(angle % 360)
 
+                                                                # maakt region of interest aan
+def roi(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
 
 def process_img(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
-    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
-    processed_img = cv2.GaussianBlur(processed_img, (3, 3), 0)
-
-    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 20, 5)
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)                                        # maakt van de originele image een GRAY scale foto
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)                                # laat de buitenste lijnen zien van een object
+    processed_img = cv2.GaussianBlur(processed_img, (3, 3), 0)                                              # blur die image!!!
+    vertices = np.array([[0, 400], [50, 200], [150, 150], [500, 150], [800, 400], [800, 400]], np.int32)     # bepaal de region of interest
+    processed_img = roi(processed_img, [vertices])
+    #                       edges
+    lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 10, 10)                         # berekent lijnen van de image door houghlineP
     draw_lines(original_image, lines)
-    draw_middle(img)
-    return original_image
+    cv2.imshow('sjaak', original_image)         # stuurt de foto en de coordinaten van de lijnen op
+    return processed_img
 
 
 img = cv2.imread("weg.png")
