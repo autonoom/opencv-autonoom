6fab1f0be474a5e78f1abc0a7ccdfa2a076003c0
Unknown <kelvin_muller@hotmail.com>
2017-06-22 16:22:44 +0200

    lijn break fix

--- opencv-autonoom/classes/camera.py
+++ opencv-autonoom/classes/camera.py
@@ -13,68 +13,68 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
-def detect_trafficlight(image):
-
-    red_light = False
-    green_light = False
-    yellow_light = False
-    # y camera coordinate of the target point 'P'
-    v = 0
-
-    # minimum value to proceed traffic light state validation
-    threshold = 150
-
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # detection
-    cascade_obj = detectMultiScale.detectMultiScale(
-        gray_image,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-
-    # draw a rectangle around the objects
-    for (x_pos, y_pos, width, height) in cascade_obj:
-        cv2.rectangle(image, (x_pos + 5, y_pos + 5), (x_pos + width - 5, y_pos + height - 5), (255, 255, 255), 2)
-        v = y_pos + height - 5
-
-        # traffic lights
-
-        roi = gray_image[y_pos + 10:y_pos + height - 10, x_pos + 10:x_pos + width - 10]
-        mask = cv2.GaussianBlur(roi, (25, 25), 0)
-        (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
-
-        # check if light is on
-        if maxVal - minVal > threshold:
-            cv2.circle(roi, maxLoc, 5, (255, 0, 0), 2)
-
-            # Red light
-            if 1.0 / 8 * (height - 30) < maxLoc[1] < 4.0 / 8 * (height - 30):
-                cv2.putText(image, 'Red', (x_pos + 5, y_pos - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-                red_light = True
-                s.send('stop')
-                print "red"
-
-            # Green light
-            elif 5.5 / 8 * (height - 30) < maxLoc[1] < height - 30:
-                cv2.putText(image, 'Green', (x_pos + 5, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                green_light = True
-                s.send('start')
-                print "green"
-
-            #yellow light
-            elif 4.0/8*(height-30) < maxLoc[1] < 5.5/8*(height-30):
-                cv2.putText(image, 'Orange', (x_pos+5, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
-                yellow_light = True
-                # TCP_IP = '192.168.42.1'
-                # TCP_PORT = 5005
-                # BUFFER_SIZE = 1024
-                # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                # s.connect((TCP_IP, TCP_PORT))
-                # s.send('stop')
-                print "orange"
-    return image
+# def detect_trafficlight(image):
+#
+#     red_light = False
+#     green_light = False
+#     yellow_light = False
+#     # y camera coordinate of the target point 'P'
+#     v = 0
+#
+#     # minimum value to proceed traffic light state validation
+#     threshold = 150
+#
+#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#
+#     # detection
+#     cascade_obj = detectMultiScale.detectMultiScale(
+#         gray_image,
+#         scaleFactor=1.1,
+#         minNeighbors=5,
+#         minSize=(30, 30)
+#     )
+#
+#     # draw a rectangle around the objects
+#     for (x_pos, y_pos, width, height) in cascade_obj:
+#         cv2.rectangle(image, (x_pos + 5, y_pos + 5), (x_pos + width - 5, y_pos + height - 5), (255, 255, 255), 2)
+#         v = y_pos + height - 5
+#
+#         # traffic lights
+#
+#         roi = gray_image[y_pos + 10:y_pos + height - 10, x_pos + 10:x_pos + width - 10]
+#         mask = cv2.GaussianBlur(roi, (25, 25), 0)
+#         (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
+#
+#         # check if light is on
+#         if maxVal - minVal > threshold:
+#             cv2.circle(roi, maxLoc, 5, (255, 0, 0), 2)
+#
+#             # Red light
+#             if 1.0 / 8 * (height - 30) < maxLoc[1] < 4.0 / 8 * (height - 30):
+#                 cv2.putText(image, 'Red', (x_pos + 5, y_pos - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+#                 red_light = True
+#                 s.send('stop')
+#                 print "red"
+#
+#             # Green light
+#             elif 5.5 / 8 * (height - 30) < maxLoc[1] < height - 30:
+#                 cv2.putText(image, 'Green', (x_pos + 5, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+#                 green_light = True
+#                 s.send('start')
+#                 print "green"
+#
+#             #yellow light
+#             elif 4.0/8*(height-30) < maxLoc[1] < 5.5/8*(height-30):
+#                 cv2.putText(image, 'Orange', (x_pos+5, y_pos - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+#                 yellow_light = True
+#                 # TCP_IP = '192.168.42.1'
+#                 # TCP_PORT = 5005
+#                 # BUFFER_SIZE = 1024
+#                 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#                 # s.connect((TCP_IP, TCP_PORT))
+#                 # s.send('stop')
+#                 print "orange"
+#     return image
 
 
 def draw_lines(img, lines):
@@ -123,21 +123,21 @@ def draw_lines(img, lines):
     except Exception:
         pass
 
-def draw_circles(img):
-    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
-    circles = np.uint16(np.around(circles))
-
-    for i in circles[0, :]:
-        if i[2] < 200:
-            rad = (i[2] + rad) / 2
-            x = (i[0] + x) / 2
-            y = (i[1] + y) / 2
-
-            # draw the outer circle
-            cv2.circle(img, (y, x), rad, (0, 255, 0), 2)
-            # draw the center of the circle
-            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
-            # print i
+# def draw_circles(img):
+#     circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+#     circles = np.uint16(np.around(circles))
+#
+#     for i in circles[0, :]:
+#         if i[2] < 200:
+#             rad = (i[2] + rad) / 2
+#             x = (i[0] + x) / 2
+#             y = (i[1] + y) / 2
+#
+#             # draw the outer circle
+#             cv2.circle(img, (y, x), rad, (0, 255, 0), 2)
+#             # draw the center of the circle
+#             cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+#             # print i
 
 
 def draw_middle(image, gem):
@@ -152,9 +152,11 @@ def draw_middle(image, gem):
     # BUFFER_SIZE = 1024
     # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # s.connect((TCP_IP, TCP_PORT))
-
-    s.send(str(dif))
-    print dif
+    if calculate_avg(dif):
+        print 'stuur avg'
+    else:
+        pass
+    #print dif
     cv2.line(image, ((x / 2), y), ((x/2), y-50), [85, 26, 139], 1)
 
 
@@ -166,16 +168,49 @@ def calculate_degree(point):                     # http://wikicode.wikidot.com/g
     angle = angle * -1
     print round(angle % 360)
 
+counter = 0
+counter2= 0
+data = 0
+data2 =0
 
-def bird_eye (image):
-    # y, x, ch = image.shape
-    # print x
-    # print y
-    src = np.float32([[0, 140], [480, 140], [480, 360], [0, 360]])
-    dst = np.float32([[0, 0], [480, 0], [300, 450], [180, 450]])
-    M = cv2.getPerspectiveTransform(src, dst)
-    warped_image = cv2.warpPerspective(image, M, (640, 480))
-    cv2.imshow('transform', warped_image)
+
+def calculate_avg(diference):
+    global counter
+    global counter2
+    global data, data2
+    avg1 = 10
+    avg2 = 5
+    if counter < avg1:
+        counter += 1
+        data += float(diference)
+    elif counter is avg1:
+        # avg from avg
+        data = data / 100
+        if counter2 < avg2:
+            counter2 += 1
+            data2 += data
+        elif counter2 is avg2:
+            data2 = float(data2) / 5
+            print "Avg data = " + str(data2)
+            data2 *= 2
+            s.send(str(data2))
+            data = 0
+            counter = 0
+            data2 = 0
+            counter2 = 0
+            return True
+    else:
+        print 'dd'
+    return False
+# def bird_eye (image):
+#     # y, x, ch = image.shape
+#     # print x
+#     # print y
+#     src = np.float32([[0, 140], [480, 140], [480, 360], [0, 360]])
+#     dst = np.float32([[0, 0], [480, 0], [300, 450], [180, 450]])
+#     M = cv2.getPerspectiveTransform(src, dst)
+#     warped_image = cv2.warpPerspective(image, M, (640, 480))
+#     cv2.imshow('transform', warped_image)
 
 # maakt region of interest aan
 
@@ -212,10 +247,10 @@ def main():
             jpg = bytes[a:b + 2]
             bytes = bytes[b + 2:]
             frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-            new2_screen = detect_trafficlight(frame)
+            #new2_screen = detect_trafficlight(frame)
             new_screen = process_img(frame)                                   # stuurt frame op naar process_img
             cv2.imshow('window', new_screen)                                        # laat video zien
-            cv2.imshow('window2', new2_screen)
+            #cv2.imshow('window2', new2_screen)
             # cv2.imshow('wiqndow2', cap)
             cv2.imshow('window3', frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):                         # wanneer 'q' is ingedrukt stop programma
